a72551915f2d0e76d187dcb3fc339319baae11ee
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-06 15:32:47 +0500

    attach firebase

--- firebase-Authantication/App.js
+++ firebase-Authantication/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase'
+import { Header } from './src/components/common'
 
 export default class App extends React.Component {
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCXs2qMUazezjisKUS2ICNAKbasCkJdGDQ",
+      authDomain: "react-sample-17caa.firebaseapp.com",
+      databaseURL: "https://react-sample-17caa.firebaseio.com",
+      projectId: "react-sample-17caa",
+      storageBucket: "react-sample-17caa.appspot.com",
+      messagingSenderId: "411422279834"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Header headerText={'Authantication'} />
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        {/*<Text>Shake your phone to open the developer menu.</Text>*/}
       </View>
     );
   }
