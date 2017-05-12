a6da48baa84ad9989fa4113b3469dd6774614b36
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-12 14:04:23 +0500

    Import/Export

--- firebase-Authantication/src/components/common/index.js
+++ firebase-Authantication/src/components/common/index.js
@@ -1,7 +1,5 @@
 export * from './Header'
-// export * from './List'
 export * from './Card'
 export * from './CardSection'
 export * from './Button'
-export * from './Input'
-// import {} from './home'
+export * from './Spinner'
